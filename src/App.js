69d7fdb50d4653bdec6d4ed1513d5550c0095459
Lawrence J <lawrenceg563@gmail.com>
2021-08-21 13:29:39 +0530

    created header component with app icon name and navigation options created about home and posts page componentsimplemented routing to pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+
+import { Switch, Route } from "react-router-dom";
+import Header from "./components/header/header.component";
+import HomePage from "./pages/homepage/home-page.component";
+import AboutPage from "./pages/about/about-page.component";
+import PostsPage from "./pages/posts/posts-page.component";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/about" component={AboutPage} />
+          <Route path="/posts" component={PostsPage} />
+        </Switch>
     </div>
   );
 }
---
+++ src/pages/posts/posts-page.component.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const PostsPage =() =>{
+    return(
+        <div>We should have posts here in a short time</div>
+    )
+}
+
+export default PostsPage;
